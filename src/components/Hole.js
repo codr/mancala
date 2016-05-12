35dd551d4465963257d435028f53fbfa18049f75
Cody Coljee-Gray <cody.cg@gmail.com>
2016-05-12 13:25:24 +1000

    direction on button so beads render correctly.

--- src/components/Hole.js
+++ src/components/Hole.js
@@ -5,7 +5,7 @@ import AnimatedBucket from '../components/AnimatedBucket';
 export default class Hole extends Component {
 
   render () {
-    const {onClick, value, active, setBucketRef} = this.props;
+    const {onClick, value, active, setBucketRef, ...props} = this.props;
     const className = classNames(this.props.className, {
       disabled: !active,
       'btn-primary': active,
@@ -13,6 +13,7 @@ export default class Hole extends Component {
     })
     return (
       <button
+        {...props}
         className={className}
         onClick={(value !== 0) && active && onClick}>
         <AnimatedBucket
